b4045a75dcae3467a881d1f5c001e6817d969afa
wanghaozhe <2912528562@qq.com>
2020-04-03 22:03:55 +0800

    教练查询,排课管理

--- gym/src/main/java/com/zhiyou/gym/controller/GymCabinetController.java
+++ gym/src/main/java/com/zhiyou/gym/controller/GymCabinetController.java
@@ -3,19 +3,21 @@ package com.zhiyou.gym.controller;
 import com.zhiyou.gym.pojo.Gym_Cabinet;
 import com.zhiyou.gym.service.GymCabinetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-@RestController
+@Controller
 @RequestMapping(value = "Cabinet")
 public class GymCabinetController {
     @Autowired
     GymCabinetService gymCabinetService;
     @RequestMapping(value = "findAll")
-    public List<Gym_Cabinet> findAll(){
-        return gymCabinetService.findAll();
+    public String findAll(){
+        gymCabinetService.findAll();
+        return "cabinet.html";
     }
    @RequestMapping(value = "add")
     public void  add(Gym_Cabinet gymcabinet){
